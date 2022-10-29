8b0eefc0a0d5f807c495eaf59151a84317e3aa53
maotterson <otterson.2@gmail.com>
2022-10-29 01:09:33 -0400

    dump data in json to main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,5 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.trips = this.tripService.getTrips();
-    console.log(this.trips);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  ROUTES_API_URL: "http://localhost:8080/api/v1/"
+  ROUTES_API_URL: "http://localhost:8080/api/v1"
 };
 
 /*
