f4e361fb349c3ac07026292bcde87d7b242152a2
maotterson <otterson.2@gmail.com>
2022-10-30 01:22:05 -0400

    add components for CRUD operation modals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TripComponent } from './components/trip/trip.component';
+import { NewTripComponent } from './components/newtrip/newtrip.component';
+import { EditTripComponent } from './components/edit-trip/edit-trip.component';
+import { DeleteTripComponent } from './components/delete-trip/delete-trip.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TripComponent,
+    NewTripComponent,
+    EditTripComponent,
+    DeleteTripComponent,
   ],
   imports: [
     BrowserModule,
